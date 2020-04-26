45b50c7d7b0df76ee7d4535ca6cd56c09cac5e25
Hansong Wang <hansongwang@outlook.com>
2020-04-26 11:26:06 -0400

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,18 +3,23 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "fc2559cb09e18b9a60748ff9ecd58369",
 "/": "fc2559cb09e18b9a60748ff9ecd58369",
-"main.dart.js": "f497589e02f4ce79febb86ea50e36679",
+"main.dart.js": "3210573f3b29b98ec8d686a9748ae98b",
 "favicon.png": "4b309aa90ea597ce9ff99878f95d273f",
 "icons/Icon-192.png": "4b309aa90ea597ce9ff99878f95d273f",
 "icons/Icon-512.png": "4b309aa90ea597ce9ff99878f95d273f",
 "manifest.json": "e6e97a7a60723df961614f3b7c61541e",
-"assets/LICENSE": "358a28d6fc3a4c21802125eef462c17b",
+"assets/LICENSE": "c34f8622750f925424e1f5de172d44dc",
+"assets/images/icon.png": "c79f790aeed69a854eaa56ac0e5bc2a1",
+"assets/images/theme.png": "cafb6ec026d0b5b5144def08fcee54b9",
+"assets/images/dreamhouse.gif": "b7f5605b1c0c65cfb8de788b5e79f5d1",
 "assets/images/dreamhouse_color.jpg": "35425f2bcbc2259675027d5185f7c8e5",
 "assets/images/profile.png": "e877eafbc75505dc2fa8d635a8a2cce1",
 "assets/images/dreamhouse.jpg": "7939fb9febca9070d72837f3d9480abe",
-"assets/AssetManifest.json": "07e5fbd978fabd85eb4fe87d410e6406",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/AssetManifest.json": "a92ae290a07240f0d5ce4c9ba2394659",
+"assets/videos/dreamhouse.mp4": "ed720331d3acba337f27d7d0b45a699b",
+"assets/FontManifest.json": "96880f5cbd12a15751331cdbdac93202",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/open_iconic_flutter/assets/open-iconic.woff": "3cf97837524dd7445e9d1462e3c4afe2",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
