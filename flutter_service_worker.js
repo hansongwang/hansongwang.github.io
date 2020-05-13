408d8cb7aa254c5e1892ba7a0e15750005d832f8
Hansong Wang <hansongwang@outlook.com>
2020-05-13 18:36:45 -0400

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,14 +1,16 @@
 'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "fb1e99766a2c76789a6ba2fa25a919c0",
 "/": "fb1e99766a2c76789a6ba2fa25a919c0",
-"main.dart.js": "266cce7b0c4074464cb33437a11100d3",
+"main.dart.js": "e241afcf0d8e756fd07ec1e636a5fb01",
 "favicon.png": "0c5b0bc26726f87e86ed7442ad02c76a",
 "icons/Icon-192.png": "534fa97dabc788cbfe346c364cd10af3",
 "icons/Icon-512.png": "3e986fc1c7d67ee9f10b208c171276ea",
 "manifest.json": "e6e97a7a60723df961614f3b7c61541e",
-"assets/LICENSE": "aa46f016bc799f25ddb643b3f2f71ae9",
+"assets/LICENSE": "d1cb0ff0e1964be70b7c500e5b4b1ef0",
 "assets/images/logo_black.png": "3e900b167b3449648d09ab7807a5c5e5",
 "assets/images/theme.png": "cafb6ec026d0b5b5144def08fcee54b9",
 "assets/images/dreamhouse.gif": "b7f5605b1c0c65cfb8de788b5e79f5d1",
@@ -18,7 +20,10 @@ const RESOURCES = {
 "assets/images/under_construction.gif": "442f2fade2d4d49a21d19a3a5f5c4ed5",
 "assets/images/profile.png": "b4714066ff94737077807fb802d90ea2",
 "assets/images/logo_red.png": "f2e966c847c56ecde7bd05bdce43afb7",
-"assets/AssetManifest.json": "988c11de849bc0843451241d2b4beda4",
+"assets/AssetManifest.json": "52c40773c4fb5676a285d0979a974a8b",
+"assets/lang/zh.json": "bfa02ecfc35de37c18147fd01b6f6860",
+"assets/lang/en.json": "a08c2b5af7cc340acdf7574a846bdcdd",
+"assets/lang/fr.json": "57bbe4afd6553dad05130af0d2bd4115",
 "assets/videos/dreamhouse.mp4": "ed720331d3acba337f27d7d0b45a699b",
 "assets/FontManifest.json": "96880f5cbd12a15751331cdbdac93202",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
@@ -26,26 +31,103 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheName) {
-      return caches.delete(cacheName);
-    }).then(function (_) {
-      return caches.open(CACHE_NAME);
-    }).then(function (cache) {
-      return cache.addAll(Object.keys(RESOURCES));
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "main.dart.js",
+"/",
+"index.html",
+"assets/LICENSE",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(CORE);
     })
   );
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
         }
-        return fetch(event.request);
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // If the URL is not the the RESOURCE list, skip the cache.
+  if (!RESOURCES[key]) {
+    return event.respondWith(fetch(event.request));
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
       })
+    })
   );
 });
+
